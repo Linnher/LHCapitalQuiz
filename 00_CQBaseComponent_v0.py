cab447b5d14cd3e367bc15dba81cdedd0dca1b6f
Linnher <107924149+Linnher@users.noreply.github.com>
2022-08-08 11:51:39 +1200

    Version Zero of Base Component - Skeleton Setup

---
+++ 00_CQBaseComponent_v0.py
@@ -0,0 +1,12 @@
+
+# Base component for the Capital Cities Quiz
+# Version Zero - Skeleton Setup
+
+# Author - Linn Herzig
+# LH 2022
+
+# Import libraries below this line **************************
+
+# All Functions below this line ******************************
+
+# Main Routine below this line ******************************
