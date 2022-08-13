80ab7dcda013e5df9e704850b267b882d561347a
Linnher <107924149+Linnher@users.noreply.github.com>
2022-08-13 14:37:12 +1200

    Version One of Base Component - Added YesNoChecker

---
+++ 00_CQBaseComponent_v1.py
@@ -0,0 +1,41 @@
+# Base component for the Capital Cities Quiz
+# Version One - Adding in the Yes / No Checker
+
+# Author - Linn Herzig
+# LH 2022
+
+# Import libraries below this line **************************
+
+# All Functions below this line ******************************
+def yesno(question):
+    valid = False
+    while not valid:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            response = "yes"
+            return response
+
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
+
+        else:
+            print("This is an invalid value. Please answer yes or no.")
+
+
+def instructions():
+    print()
+    print("**** How to Play ****")
+    print(" - Every round consists of five questions.")
+    print(" - Each question will have three possible answers either a, b or c.")
+    print(" - To answer the question please press the corresponding letter on the keyboard.")
+    return""
+
+
+# Main Routine below this line ******************************
+print("Welcome to the Capital Cities Quiz Game!")
+show_instructions = yesno("Would you like to view the instructions? ")
+
+if show_instructions == "yes":
+    instructions()
